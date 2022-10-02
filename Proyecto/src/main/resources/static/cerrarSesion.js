ae6c9433201a1110b7e6f8e5b48152dae97fe125
Manuel Briñez <91702513+Josand8@users.noreply.github.com>
2022-10-02 18:21:50 -0500

    Delete Proyecto directory

--- Proyecto/src/main/resources/static/cerrarSesion.js
+++
@@ -1,7 +0,0 @@
-function cerrarSesion() {
-    var nameRegistro = "";
-    var passwordRegistro = "";
-    localStorage.removeItem('nombre_usuario_recibido');
-    localStorage.clear();
-    location.reload();
-}
